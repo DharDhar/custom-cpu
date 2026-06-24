87f0c2e9b9402e407a9cf26f6767a99eb4ff6822
DharDhar <dhardhar2006@gmail.com>
2026-06-24 19:42:14 +0530

    instruction str matching

--- assembler/assembler.c
+++ assembler/assembler.c
@@ -1,67 +1,174 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
+#include <stdbool.h>
 #include "instructions.h"
 
-/*  TODO: implement label count limit
+/*  TODO: make head_node static
  *		  refactor error messages
- *
+ *		  
  */
 
 #define BUFFER_SIZE 50
-#define LABEL_SIZE 20
+#define MAX_LABEL_SIZE 7
+#define MAX_LABEL_COUNT 20
+#define MAX_INSTRUCTIONS 10
+
+#define FILE_OPEN_ERROR 1
+#define LABEL_TOO_WIDE 2
+#define TOO_MANY_LABELS 3
+#define TOO_MANY_INSTRUCTIONS 4
+#define SYMBOLS_IN_LABEL 5
 
 typedef struct Node {
-	char label_name[LABEL_SIZE+1];
+	char label_name[MAX_LABEL_SIZE+1];
 	int line;
+	int instruction_num;
 	struct Node* next;
 } Node;
+
+void throw_error(int error_no, FILE *fp);
+void create_label_node(Node **current_node);
+void label_scraper(Node *head_node_ptr, FILE *fp);
+bool is_line_label(int line_num, Node *head_node);
 
 int main(void)
 {
 	FILE *fp = fopen("program.txt", "r");
+	Node head_node = {{0}, 0, 0, NULL};
+
 	char buf[BUFFER_SIZE] = {0};
-	Node head_node = {{0}, 0,NULL};
-	Node * current_node = &head_node;
-	int i, line_num = 1;
+	int line_num = 1;
+	char *instr = {0};
+	bool match_found = false;
 
 	if (fp == NULL)
-	{
-		perror("Error opening file");
-		return 1;
-	}	
+		throw_error(FILE_OPEN_ERROR, fp);
 
-	while(fgets(buf, BUFFER_SIZE - 1, fp) != NULL)
-	{
-		for(i = 0; i < BUFFER_SIZE; i++)
-			if (buf[i] == ':')
-			{
-					if (i > LABEL_SIZE + 1)
-					{
-						printf("Label cannot be greater than %d characters wide\n", LABEL_SIZE);
-						fclose(fp);
-						return 1;
-					}
-				buf[i] = '\0'; 
-				strcpy(current_node->label_name, buf);
-				current_node->line = line_num;
-				current_node->next = calloc(1, sizeof(Node));
-				current_node = current_node->next;
-				current_node->next = NULL;
-			}
-		line_num++;
-	}
-	
-	current_node = &head_node;
-	do
-	{
-		printf("%s %d\n", current_node->label_name, current_node->line);
-		current_node = current_node->next;
-	}
-	while (current_node->next != NULL);
-		
-
+	label_scraper(&head_node, fp);
 	fclose(fp);
 
+	fp = fopen("program.txt", "r");
+
+	while(fgets(buf, BUFFER_SIZE, fp) != NULL)
+	{
+		if (!isspace(buf[3]) && !is_line_label(line_num, &head_node)) //since 3rd char is empty
+			printf("Error %d\n", line_num);							  //for legal instructions
+		buf[3] = '\0';
+		match_found = false;
+		for (int i = 0; i < INSTRUCTION_COUNT; i++)
+			if (strcmp(buf, instr_str[i].string) == 0)
+			{
+				printf("Match found %.3b\n", instr_str[i].opcode);
+				match_found = true;
+				break;
+			}
+		if (!match_found && !is_line_label(line_num, &head_node))
+			printf("Unknown instruction at line %d\n", line_num);
+		line_num++;
+	}
+
+	fclose(fp);
 	return 0;
+}
+
+void label_scraper(Node *head_node_ptr, FILE *fp)
+{
+	Node * current_node = head_node_ptr;
+	char buf[BUFFER_SIZE] = {0};
+	int line_num = 1;
+	int label_count = 0;
+	int instruction_num = 0;
+	int error = 0;
+	bool symbols_in_label = false;
+	bool is_label = false;
+
+
+	while(fgets(buf, BUFFER_SIZE, fp) != NULL)
+	{
+		for(int i = 0; i < BUFFER_SIZE; i++)
+		{
+			if (buf[i] == ':')
+			{
+				for (int j = 0; j < i; j++)
+				{
+					if (!isalnum(buf[j]) && buf[j] != '_')
+					{
+						symbols_in_label = true;	
+						printf("At line %d\n", line_num);
+						printf("%s\n", buf);
+					}
+//printf("Here");
+				}
+
+				if (i > MAX_LABEL_SIZE && !symbols_in_label)
+				{
+					printf("At line %d\n", line_num);
+					printf("%s\n", buf);
+					error = LABEL_TOO_WIDE;
+				} else if (label_count == MAX_LABEL_COUNT)
+					error = TOO_MANY_LABELS;
+
+				buf[i] = '\0'; 
+				
+				is_label = true;
+				label_count++;
+				strcpy(current_node->label_name, buf); //(dest, src)
+				current_node->line = line_num;
+				current_node->instruction_num = instruction_num;
+				create_label_node(&current_node);
+			}
+		}
+ 
+		line_num++;
+		if (!is_label)
+			instruction_num++;
+		is_label = false;
+	}
+
+	if (symbols_in_label)
+		error = SYMBOLS_IN_LABEL;
+
+	if (error)
+		throw_error(error, fp);
+
+	return;
+}
+
+bool is_line_label(int line_num, Node *head_node)
+{
+	Node *current_node = head_node;
+
+	while(1)
+	{
+		if (line_num == current_node->line)
+			return true;
+		if (current_node->next == NULL)
+			break;
+		else
+			current_node = current_node->next;
+	}
+	return false;
+}
+
+void throw_error(int error_no, FILE *fp)
+{
+	switch(error_no) {
+		case FILE_OPEN_ERROR: printf("Error opening program file\n"); break;
+		case LABEL_TOO_WIDE: printf("Labels cannot be more than %d characters wide\n", MAX_LABEL_SIZE); break;
+		case TOO_MANY_LABELS: printf("Number of labels cannot exceed %d\n", MAX_LABEL_COUNT); break;
+		case SYMBOLS_IN_LABEL: printf("Labels cannot have symbols or spaces\n"); break;
+	}
+
+	fclose(fp);
+	exit(EXIT_FAILURE);
+}
+
+void create_label_node(Node **current_node)
+{
+	(*current_node)->next = calloc(1, sizeof(Node));
+	*current_node = (*current_node)->next;
+	(*current_node)->next = NULL;
+	return;
 }
--- assembler/instructions.h
+++ assembler/instructions.h
@@ -1,21 +1,32 @@
 //Contains the assembly instructions and a short description for each
+#define INSTRUCTION_COUNT 22
 
-const char *instr_str[] = {"ldr", //load to register
-						   "ldm", //load to memory
-						   "adc", //add with carry
-						   "sbc", //subtract with carry
-						   "and", //bitwise and of registers
-						   "orr", //bitwise or of registers
-						   "xor", //bitwise ex-or of registers
-						   "ars", //arithmetic right shift a register
-						   "lrs", //logical right shift a register
-						   "lls", //logical left shift a register
-						   "psh", //push to stack
-						   "pop", //pop from stack
-						   "jmr", //jump to register val
-						   "jmi", //jump to immediate
-						   "bzc", //branch if zero flag clear
-						   "bzs", //branch if zero flag set
-						   "bcc", //branch if carry flag clear
-						   "bcs"};//branch if carry flag set
-								
+typedef struct {
+	char *string;
+	int opcode;
+} Instruction;
+
+const Instruction instr_str[] = {{"ldr", 0b000}, //load to register
+		 					     {"ldm", 0b001}, //load to memory
+							     {"adc", 0b010}, //add with carry
+						   		 {"sbc", 0b010}, //subtract with carry
+						   		 {"and", 0b010}, //bitwise and
+						   		 {"orr", 0b010}, //bitwise or
+						   		 {"xor", 0b010}, //bitwise ex-or
+						   		 {"ars", 0b010}, //arithmetic right shift
+						   		 {"lrs", 0b010}, //logical right shift
+						   		 {"lls", 0b010}, //logical left shift
+						   		 {"psh", 0b011}, //push to stack
+						   		 {"pop", 0b011}, //pop from stack
+						   		 {"jmi", 0b100}, //jump to immediate
+						   		 {"jmr", 0b101}, //jump to register val
+						   		 {"bzc", 0b101}, //branch if zero flag clear
+						   		 {"bzs", 0b101}, //branch if zero flag set
+						   		 {"bcc", 0b101}, //branch if carry flag clear
+						   		 {"bcs", 0b101}, //branch if carry flag set
+								 {"sec", 0b110}, //set carry flag
+								 {"clc", 0b110}, //clear carry flag
+								 {"sez", 0b110}, //set zero flag
+								 {"clz", 0b110}};//clear zero flag
+		
+
